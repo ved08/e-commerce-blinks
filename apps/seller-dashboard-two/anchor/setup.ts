cbb9f2055533543a601b39a211688511ce6fd192
ved08 <vedvardhan.g@gmail.com>
2024-08-29 21:53:37 +0530

    created new next app

---
+++ apps/seller-dashboard-two/anchor/setup.ts
@@ -0,0 +1,21 @@
+import { IdlAccounts, Program } from "@coral-xyz/anchor";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import idl from "./idl.json";
+import type { EcomEscrow } from "./type";
+import { getConnection } from "../../seller-dashboard/@/lib/constants";
+
+export const programId = new PublicKey(
+  "9pJ1GXPbXQPUvzLJsz3HVSLqQ1rEyxauvZDL5e9aBRx8"
+);
+
+const connection = getConnection();
+
+export const program = new Program(idl as EcomEscrow, {
+  connection,
+});
+
+export type todoState = IdlAccounts<EcomEscrow>["order"];
---
+++ apps/seller-dashboard-two/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@repo/ui/button'
+import React from 'react'
+import Navbar from '../../../../seller-dashboard/@/components/navbar/navbar'
+
+export default function Dashboard() {
+  return (
+    <div className='bg-blue-900'>
+      <Navbar />
+    </div>
+  )
+}
